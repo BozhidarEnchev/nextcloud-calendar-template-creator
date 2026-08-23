9f278f878429d98d1ba0e98dc5131e2098fba1d5
Bozhidar Enchev <bozhidar.enchev03@gmail.com>
2026-08-23 16:00:27 +0300

    fix: create data directory before the db engine

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
@@ -6,6 +6,8 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 class Base(DeclarativeBase):
     pass
 
+if "data" not in os.listdir(os.getcwd()):
+    os.mkdir("data")
 
 engine = create_engine("sqlite:///data/app.db")
 SessionLocal = sessionmaker(
