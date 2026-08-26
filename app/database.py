490920e933f1b8eb7841613928afd6500155136d
Bozhidar Enchev <bozhidar.enchev03@gmail.com>
2026-08-26 13:32:58 +0300

    feat: switch to postgresql

--- app/database.py
+++ app/database.py
@@ -1,6 +1,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from decouple import config
 
 
 class Base(DeclarativeBase):
@@ -9,7 +10,9 @@ class Base(DeclarativeBase):
 if "data" not in os.listdir(os.getcwd()):
     os.mkdir("data")
 
-engine = create_engine("sqlite:///data/app.db")
+engine = create_engine(
+    f"postgresql+psycopg2://{config('PG_USER')}:{config('PG_PASSWORD')}@{config('PG_ADDRESS')}:{config('PG_PORT')}/{config('PG_DB')}"
+)
 SessionLocal = sessionmaker(
     bind=engine,
     autoflush=False,
