595f751f6ff6b7eb1620767bc83aa7cc2342d619
Bozhidar Enchev <bozhidar.enchev03@gmail.com>
2026-09-10 21:16:23 +0300

    feat: add alembic for db migrations

--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-from contextlib import asynccontextmanager
-
 from decouple import config
 from fastapi import Depends, FastAPI, Request
 from fastapi.responses import HTMLResponse
@@ -8,7 +6,7 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 from starlette.middleware.sessions import SessionMiddleware
 
-from app.database import Base, engine, get_db
+from app.database import get_db
 from app.dependencies import require_login
 from app.models.event_template import EventTemplate
 from app.models.user import User
@@ -17,14 +15,7 @@ from .routers import auth, event_templates, events, nextcloud_account
 from .templating import templates
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    Base.metadata.create_all(bind=engine)
-
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.add_middleware(SessionMiddleware, secret_key=config("SECRET_KEY"))
 
